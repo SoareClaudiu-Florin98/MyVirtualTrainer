137a7ea88497a58705d65d12db63b00d4c151d0f
Soare Claudiu <soareclaudiuflorin@gmail.com>
2021-06-18 23:56:45 +0300

    before and after design

--- Frontend/myvirtualtrainer/src/components/BeforeAndAfter/BeforeAndAfter.js
+++ Frontend/myvirtualtrainer/src/components/BeforeAndAfter/BeforeAndAfter.js
@@ -1,32 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import "./BeforeAndAfter.css";
-import {
-    Container,
-    Row,
-    Col,
-} from "react-bootstrap";
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
+
+import photo from "../Assets/photo.svg";
+
 const BeforeAndAfter = () => {
+  const [beforeImg, setBeforeImg] = useState("");
+  const [afterImg, setAfterImg] = useState("");
 
-    const [beforeImg , setBeforeImg] = useState('') ; 
-    const [afterImg, setAfterImg] = useState('') ;
-
-return(
-<Container fluid>
-  <Row xs={2} md={4} lg={6} className="justify-content-md-center">
-    <Col>Before</Col>
-    <Col>After</Col>
-  </Row>
-  <Row xs={2} md={4} lg={6} className="justify-content-md-center">
-    <Col>PhotoBefore</Col>
-    <Col>PhotoAfter</Col>
-  </Row>
-  <Row xs={2} md={4} lg={6} className="justify-content-md-center">
-    <Col>Generate</Col>
-  </Row>
-  <Row xs={2} md={4} lg={6} className="justify-content-md-center">
-    <Col>Poza</Col>
-  </Row>
-</Container>
-)
+  return (
+    <Container className="containerbaa" fluid>
+      <Row xs={2} md={4} lg={6} className="justify-content-md-center" id = "beforeAndAfterRow">
+        
+        <Col>
+          <h3>Before</h3>
+          <div className="beforeImg">
+            <Image src={photo} alt="PhotoBefore" rounded />
+          </div>
+        </Col>
+        <Col>
+          <h3>After</h3>
+          <div className="afterImg">
+            <Image src={photo} alt="PhotoAfter" rounded />
+          </div>
+        </Col>
+        
+      </Row>
+      <Row xs={2} md={4} lg={6} className="justify-content-md-center">
+        <Col>
+          <Button className="buttonbaf" variant="success">Generate Before And After</Button>
+        </Col>
+      </Row>
+      <Row xs={2} md={4} lg={6} className="justify-content-md-center">
+        <Col>
+          <Image src="" alt="Poza" rounded />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 export default BeforeAndAfter;
