7ddf7b27fc1d1632728808db2bbd3126571a079b
Soare Claudiu <soareclaudiuflorin@gmail.com>
2021-06-19 12:05:35 +0300

    better

--- Frontend/myvirtualtrainer/src/components/BeforeAndAfter/BeforeAndAfter.js
+++ Frontend/myvirtualtrainer/src/components/BeforeAndAfter/BeforeAndAfter.js
@@ -5,36 +5,67 @@ import { Container, Row, Col, Button, Image } from "react-bootstrap";
 import photo from "../Assets/photo.svg";
 
 const BeforeAndAfter = () => {
-  const [beforeImg, setBeforeImg] = useState("");
-  const [afterImg, setAfterImg] = useState("");
+  const [beforeImg, setBeforeImg] = useState(photo);
+  const [afterImg, setAfterImg] = useState(photo);
+  const [finalImg, setFinalImg] = useState("");
+  const [existFinalImg, setExistFinalImg] = useState(false);
+
+  const hanfleBeforeChange=(event) => {
+    const reader = new FileReader();
+    reader.onload= () => {
+      if(reader.readyState ===2){
+        setBeforeImg(reader.result) ; 
+      }
+    }
+    reader.readAsDataURL(event.target.files[0]) ; 
+    
+  }
+  const hanfleAfterChange=(event) => {
+    const reader = new FileReader();
+    reader.onload= () => {
+      if(reader.readyState ===2){
+        setAfterImg(reader.result) ; 
+      }
+    }
+    reader.readAsDataURL(event.target.files[0]) ;
+  }
+  const hanfleGenerateClick=() => {
+    setExistFinalImg(true) ; 
+
+  }
+ 
+
 
   return (
     <Container className="containerbaa" fluid>
-      <Row xs={2} md={4} lg={6} className="justify-content-md-center" id = "beforeAndAfterRow">
+      <Row xs={2} sm = {2} md={4} lg={6}   className="justify-content-md-center" id = "beforeAndAfterRow">
         
-        <Col>
+        <Col >
           <h3>Before</h3>
           <div className="beforeImg">
-            <Image src={photo} alt="PhotoBefore" rounded />
+            <img src={beforeImg} alt="PhotoBefore" rounded id="beforeImgCanvas"/>          
           </div>
+
+          <input type="file" id = "input" onChange={hanfleBeforeChange} accept="image/png, image/jpeg"></input>
         </Col>
         <Col>
           <h3>After</h3>
           <div className="afterImg">
-            <Image src={photo} alt="PhotoAfter" rounded />
+            <img src={afterImg} alt="PhotoAfter" rounded id="afterImgCanvas"/>          
           </div>
+          <input type="file" id = "input" onChange={hanfleAfterChange} accept="image/png, image/jpeg"></input>
         </Col>
         
       </Row>
-      <Row xs={2} md={4} lg={6} className="justify-content-md-center">
+      <Row xs={2} sm = {2} md={4} lg={6} className="justify-content-md-center">
         <Col>
-          <Button className="buttonbaf" variant="success">Generate Before And After</Button>
+          <Button className="buttonbaf" variant="success" onClick={hanfleGenerateClick}>Generate Before And After</Button>
         </Col>
       </Row>
-      <Row xs={2} md={4} lg={6} className="justify-content-md-center">
-        <Col>
-          <Image src="" alt="Poza" rounded />
-        </Col>
+      <Row xs={6} sm = {2} md={6} lg={6} className="justify-content-md-center">
+        {beforeImg !=photo &&afterImg !=photo&& existFinalImg&&<Col>
+          <img src="" alt="Poza" rounded />
+        </Col>}
       </Row>
     </Container>
   );
