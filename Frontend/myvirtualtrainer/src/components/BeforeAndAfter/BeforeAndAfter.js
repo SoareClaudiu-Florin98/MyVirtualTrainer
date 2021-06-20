205108d39e99d5c27a75915686522b650e114679
Soare Claudiu <soareclaudiuflorin@gmail.com>
2021-06-21 00:37:11 +0300

    before and after

--- Frontend/myvirtualtrainer/src/components/BeforeAndAfter/BeforeAndAfter.js
+++ Frontend/myvirtualtrainer/src/components/BeforeAndAfter/BeforeAndAfter.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import "./BeforeAndAfter.css";
-import { Container, Row, Col, Button, Image } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 import photo from "../Assets/photo.svg";
 
 const BeforeAndAfter = () => {
   const [beforeImg, setBeforeImg] = useState(photo);
   const [afterImg, setAfterImg] = useState(photo);
-  const [finalImg, setFinalImg] = useState("");
-  const [existFinalImg, setExistFinalImg] = useState(false);
-
   const hanfleBeforeChange=(event) => {
     const reader = new FileReader();
     reader.onload= () => {
@@ -27,11 +24,61 @@ const BeforeAndAfter = () => {
         setAfterImg(reader.result) ; 
       }
     }
+
     reader.readAsDataURL(event.target.files[0]) ;
   }
-  const hanfleGenerateClick=() => {
-    setExistFinalImg(true) ; 
 
+  function download(canvas, filename) {
+
+    var lnk = document.createElement('a'), e;
+
+    lnk.download = filename;
+
+    lnk.href = canvas.toDataURL("image/png;base64");
+    if (document.createEvent) {
+      e = document.createEvent("MouseEvents");
+      e.initMouseEvent("click", true, true, window,
+                       0, 0, 0, 0, 0, false, false, false,
+                       false, 0, null);
+  
+      lnk.dispatchEvent(e);
+    } else if (lnk.fireEvent) {
+      lnk.fireEvent("onclick");
+    }
+  }
+
+  const handleDownloadClick=() => {
+    let canvas = document.getElementById('canvasResult')
+    download(canvas, 'myimage.png');
+
+  }
+
+
+  const hanfleGenerateClick=() => {
+
+    if(beforeImg !== photo && afterImg !== photo){
+      let canvas = document.getElementById('canvasResult') ; 
+      const imgBeforeForCanvas = document.getElementById('beforeImgCanvas') ; 
+      const imgAfterForCanvas = document.getElementById('afterImgCanvas') ;          
+      canvas.width = imgBeforeForCanvas.width*2;
+      canvas.height = imgBeforeForCanvas.height;
+
+      var  imageBeforeDraw = new Image(imgBeforeForCanvas.width , imgBeforeForCanvas.height);
+      imageBeforeDraw.src = imgBeforeForCanvas.src; 
+      imageBeforeDraw.width = imgBeforeForCanvas.width*2 ; 
+      imageBeforeDraw.height = imgBeforeForCanvas.height ; 
+
+
+      var  imageAfterDraw = new Image(imgBeforeForCanvas.width , imgBeforeForCanvas.height);
+      imageAfterDraw.src = imgAfterForCanvas.src; 
+      imageAfterDraw.width = imgBeforeForCanvas.width*2 ; 
+      imageAfterDraw.height = imgBeforeForCanvas.height ; 
+      
+      let ctx = canvas.getContext('2d');
+      ctx.drawImage(imageBeforeDraw, 0, 0,imgBeforeForCanvas.width,imgBeforeForCanvas.height );
+      ctx.drawImage(imageAfterDraw, imgBeforeForCanvas.width, 0 ,imgBeforeForCanvas.width,imgBeforeForCanvas.height );
+      
+    }
   }
  
 
@@ -62,10 +109,15 @@ const BeforeAndAfter = () => {
           <Button className="buttonbaf" variant="success" onClick={hanfleGenerateClick}>Generate Before And After</Button>
         </Col>
       </Row>
-      <Row xs={6} sm = {2} md={6} lg={6} className="justify-content-md-center">
-        {beforeImg !=photo &&afterImg !=photo&& existFinalImg&&<Col>
-          <img src="" alt="Poza" rounded />
-        </Col>}
+      <Row xs={12} sm = {12} md={2} lg={3} className="justify-content-md-center">
+        <Col>
+          <canvas id ='canvasResult'/>
+        </Col>              
+      </Row>
+      <Row xs={2} sm = {2} md={4} lg={6} className="justify-content-md-center">
+        <Col>
+          <Button className="buttonbaf" variant="primary" onClick={handleDownloadClick} >Download</Button>
+        </Col>
       </Row>
     </Container>
   );
